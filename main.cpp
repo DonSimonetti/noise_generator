e3e09a262dd4a974f67d0f58421be930d932d25a
matteo <matt.simonetti@hotmail.com>
2021-03-03 23:27:05 +0100

    Extracted noise function

--- main.cpp
+++ main.cpp
@@ -1,23 +1,28 @@
 #include <iostream>
 #include <png++/png.hpp>
-#include <random>
+
+uint8_t noise(uint32_t pX, uint32_t pY);
 
 int main()
 {
     using namespace std;
 
-    png::image<png::rgb_pixel> image(16,16);
-
+    png::image<png::rgb_pixel> image(128,128);
 
     for(int i=0;i<image.get_height();i++)
     {
         for(int j=0;j<image.get_width();j++)
         {
-            image[i][j].red=static_cast<uint8_t>(random()%255);
+            image[i][j].red=noise(i,j);
+            image[i][j].green=noise(i,j);
         }
     }
 
     image.write("noise.png");
-
     return 0;
+}
+
+uint8_t noise(uint32_t pX, uint32_t pY)
+{
+    return random()%255;
 }
