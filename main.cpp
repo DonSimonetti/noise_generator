49391668c5f2618179dff79b3e027a9cb675f21b
matteo <matt.simonetti@hotmail.com>
2021-03-03 23:42:28 +0100

    Made noise a little bit more sophisticated

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <png++/png.hpp>
+#include <random>
 
 uint8_t noise(uint32_t pX, uint32_t pY);
 
@@ -9,12 +10,14 @@ int main()
 
     png::image<png::rgb_pixel> image(128,128);
 
+    srand(time(nullptr));
+
     for(int i=0;i<image.get_height();i++)
     {
         for(int j=0;j<image.get_width();j++)
         {
             image[i][j].red=noise(i,j);
-            image[i][j].green=noise(i,j);
+            image[i][j].blue=noise(i,j);
         }
     }
 
@@ -24,5 +27,5 @@ int main()
 
 uint8_t noise(uint32_t pX, uint32_t pY)
 {
-    return random()%255;
+    return (pX+pY)/256.0*255*(random()%20)/20;
 }
